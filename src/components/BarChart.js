092eed5153b63175449807b90f0b64c6bf87f5f8
Greg Tan <gregtan022@gmail.com>
2022-02-24 10:31:44 +0800

    imported chartjs

--- src/components/BarChart.js
+++ src/components/BarChart.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Chart as ChartJS } from 'chart.js/auto'
+import { Chart }            from 'react-chartjs-2'
 import { Bar } from 'react-chartjs-2';
 const BarChart = () => {
     
